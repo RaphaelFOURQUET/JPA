09c8502728722a6e6670a8d6156cac7504b12b99
RaphaelFOURQUET <raphikifourque@gmail.com>
2016-06-16 09:15:24 +0200

    First commit : pom error, code fourni par Nicolas (formateur).

---
+++ jpa/src/main/java/io/robusta/jpa/demo/JpaApplication.java
@@ -0,0 +1,42 @@
+package io.robusta.jpa.demo;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+import io.robusta.fora.EmFactory;
+import io.robusta.jpa.demo.entities.Caddie;
+import io.robusta.jpa.demo.entities.Category;
+import io.robusta.jpa.demo.entities.Product;
+
+public class JpaApplication {
+
+	public static void main(String[] args) {
+
+		//Category standard
+		Product ketchup = new Product("Ketchup Tomato Heinz", 2.35f);
+		Product watch = new Product("Watch", 19990f);
+		Product milk = new Product("Milk", 1.15f);
+		Product cornflakes = new Product("Corn Flakes", 1.55f);
+		Product jeans = new Product("Jeans", 20.90f);
+		Product shoes = new Product("Shoes", 15.00f);
+		Product chair = new Product("Chair", 10.00f);
+		Product hat = new Product("Hat", 70f);
+
+
+		Category csp = new Category("CSP");
+		Category luxe = new Category("luxory");
+		Category premium = new Category("premium");
+		Category standard = new Category("standard");
+		Category lowcost = new Category("low-cost");
+		Category fooding = new Category("fooding");
+		
+
+	
+		
+		
+	}
+}
---
+++ jpa/src/main/java/io/robusta/jpa/demo/entities/Product.java
@@ -0,0 +1,86 @@
+package io.robusta.jpa.demo.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+
+
+@Entity
+public class Product{
+
+	@Id
+	@GeneratedValue
+	int id;
+	
+	
+	@Size(min=2, max=46)
+	@Column(nullable=false, length=15)
+	String name;
+	
+	
+	String email;
+	
+	@Min(0)
+	
+	float price;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	Category category;
+	
+	
+	public Product() {
+	}
+
+	public Product(String name, float price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	
+
+	public int getId() {
+		return id;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	
+
+	
+	
+	
+}
